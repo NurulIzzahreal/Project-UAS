cde5f6b59997c255efa3ba627dc4bb74fe32dbd7
NuruIzzah <nurulizzahreal@gmail.com>
2023-12-21 11:15:07 +0700

    upload lagi

--- ArbainNawawiyah/views.py
+++ ArbainNawawiyah/views.py
@@ -1,6 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
 def ArbainNawawiyah(request):
   template = loader.get_template('Hadits.html')
+  return HttpResponse(template.render())
+
+  template = loader.get_template('about.html')
+  return HttpResponse(template.render())
+
+  template = loader.get_template('galery.html')
+  return HttpResponse(template.render())
+
+  template = loader.get_template('service.html')
   return HttpResponse(template.render())
