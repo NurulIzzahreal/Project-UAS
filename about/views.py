4d7f71af292ea06e4d258b98eaf74e8f2397c1bf
NuruIzzah <nurulizzahreal@gmail.com>
2023-12-25 09:37:34 +0700

    upload lagi deh

--- about/views.py
+++ about/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def ArbainNawawiyah(request):
-  template = loader.get_template('index.html')
+def about(request):
+  template = loader.get_template('about.html')
   return HttpResponse(template.render())
